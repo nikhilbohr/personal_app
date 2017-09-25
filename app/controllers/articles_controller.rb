0c5ea8a6f81ca87c7e49c4712674f3cfb950bd12
nikhilbohr <nikhil@hackerrank.com>
2017-09-25 13:59:45 +0530

    checking commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 # adding comments to test git push from vs code editor
+
 class ArticlesController < ApplicationController
 
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
